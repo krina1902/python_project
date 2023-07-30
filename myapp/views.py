d7dc2ea71469e2b706838fc7333c986a649c6f92
krina1902 <krinaantala33@gmail.com>
2023-07-30 19:21:40 +0530

    dj

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import User
+import requests
+import random
 
 # Create your views here.
 def index(request):
@@ -96,4 +98,44 @@ def profile(request):
 	else:
 		return render(request,'profile.html',{'user':user})
 
-	
+def forgot_password (request):
+	if request.method=="POST":
+		mobile=request.POST['mobile']
+		try:
+			user=User.objects.get(mobile=mobile)
+			otp=random.randint(1000,9999)
+			url = "https://www.fast2sms.com/dev/bulkV2"
+			querystring = {"authorization":"M6sYpXnKx3t9I8zQJqwa2VNGdkjuCvPLrAybBRcifH7FESge5ZsYWunxdMrvy4X60L39hkJ5FbRACEgU","variables_values":str(otp),"route":"otp","numbers":str(mobile)}
+			headers = { 'cache-control': "no-cache"}
+			response = requests.request("GET", url, headers=headers, params=querystring)
+			return render(request,'otp.html',{'otp':otp,'mobile':mobile})
+		except:
+			msg='Mobile Not Registered'
+			return render(request,'forgot-password.html',{'msg':msg})
+	else:
+		return render(request,'forgot-password.html')
+
+def verify_otp(request):
+	mobile=request.POST['mobile']
+	otp=request.POST['otp']
+	uotp=request.POST['uotp']
+	if otp==uotp:
+		return render(request,'new-password.html',{'mobile':mobile})
+	else:
+		msg="Invalid OTP"
+		return render(request,'otp.html',{'msg':msg,'otp':otp,'mobile':mobile})
+
+def new_password(request):
+	mobile=request.POST['mobile']
+	np=request.POST['npassword']
+	cnp=request.POST['cnpassword']
+
+	if np==cnp:
+		user=User.objects.get(mobile=mobile)
+		user.password=np
+		user.save()
+		msg="Password Update Successfully"
+		return render(request,'login.html',{'msg':msg})
+	else:
+		msg='New password and Confirm new password does not match'
+		return render(request,'new-password.html',{'mobile':mobile,'msg':msg})
